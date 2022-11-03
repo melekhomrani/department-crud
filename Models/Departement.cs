758d2c9a332de8e02684675b78cb488646f17ab6
melekhomrani <melekhomrani87@gmail.com>
2022-11-03 10:11:09 +0100

    departmet model

--- Models/Departement.cs
+++ Models/Departement.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ogarphia_crud.Models
+{
+    [Table("departement", Schema = "dbo")]
+    public class Departement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Department ID")]
+        public int DepartmentId { get; set; }
+        [Required]
+        [Display(Name = "Department Name")]
+        [Column(TypeName = "varchar(50)")]
+        public string DepartmentName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [Display(Name = "Department Abrreviation")]
+        public string DepartmentAbbr { get; set; }
+
+    }
+}
