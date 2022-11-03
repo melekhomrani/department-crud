be91f2bcaf9d9ab12ffb4753587ca16910432fd6
melekhomrani <melekhomrani87@gmail.com>
2022-11-03 10:52:28 +0100

    create databse based on model/ add-migration

---
+++ Models/HRDataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ogarphia_crud.Models;
+
+namespace ogarphia_crud.Models
+{
+    public class HRDataBaseContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=RYUK\SQLEXPRESS; initial catalog=HRDataBase; integrated security=true;");
+        }
+
+    }
+}
