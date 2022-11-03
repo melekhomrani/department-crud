0d5293f67081aed2a44bf30ac25ae4077d561cf4
melekhomrani <melekhomrani87@gmail.com>
2022-11-03 10:08:20 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ogarphia_crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
